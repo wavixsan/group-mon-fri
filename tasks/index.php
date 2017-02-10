efcd50bd3b9ea8b230c1f70891dd8c0dc015f534
Andrii Popov <andrii.codesushi@gmail.com>
2017-02-10 18:51:12 +0000

    task3 added

--- tasks/index.php
+++ tasks/index.php
@@ -1,2 +1,3 @@
 <a href="task_1.php">Is number a prime</a> <br>
 <a href="task_2.php">Get prime numbers from range</a>
+<a href="task_3.php">Random dimension table</a>
---
+++ tasks/task_3.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Random dimension table</title>
+</head>
+<body>
+    
+    <h1>Random dimension table</h1>
+    <?php
+    
+        $rows = rand(1, 10);
+        $cols = rand(1, 10);
+        $max_color_value = pow(16, 6);
+    ?>
+    
+    <table border="1">
+        <?php for ($i = 1; $i <= $rows; $i++): ?>
+            <tr>
+                <?php for ($j = 1; $j <= $cols; $j++): 
+                
+                    $color = dechex(rand(0, $max_color_value));
+                    ?>
+                    <td style="background-color: #<?=$color?>"><?=$color ?></td>
+                <?php endfor; ?>
+            </tr>
+        <?php endfor; ?>
+    </table>
+    
+    
+</body>
+</html>
