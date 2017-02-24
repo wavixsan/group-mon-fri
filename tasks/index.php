f47c5c5f8738d2a8d3077e2093f9c46217c74498
Andrii Popov <andrii.codesushi@gmail.com>
2017-02-24 19:00:05 +0000

    more tasks on functions

--- tasks/index.php
+++ tasks/index.php
@@ -1,3 +1,8 @@
 <a href="task_1.php">Is number a prime</a> <br>
 <a href="task_2.php">Get prime numbers from range</a>
 <a href="task_3.php">Random dimension table</a>
+<a href="task_4.php">Something about integers :)</a>
+<a href="task_5.php">Cyrrrylic</a>
+<a href="task_6.php">TOP 3 words</a>
+<a href="task_7.php">Loops</a>
+<a href="task_8.php">Recursion</a>
